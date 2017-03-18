d6f6698b9fcc4bacc09dd281a706b800369f8657
Alexander Yalov <alexander.yalov@gmail.com>
2017-03-18 03:43:08 +0300

    sidebar flex

--- scripts/GetLatestReleaseInfo.js
+++ scripts/GetLatestReleaseInfo.js
@@ -1,32 +1,31 @@
 function TimeAgo (date1, date2) {
-  var oneHour = 60 * 60 * 1000
-  var oneDay = 24 * oneHour
-  var dateDiff = date2 - date1
-  var timeAgo
+  var oneHour = 60 * 60 * 1000;
+  var oneDay = 24 * oneHour;
+  var dateDiff = date2 - date1;
+  var timeAgo;
   if (dateDiff < oneDay) {
-    hours = dateDiff / oneHour;
-    timeAgo = hours.toFixed(1) + ' hours ago'
+    var hours = dateDiff / oneHour;
+    timeAgo = hours.toFixed(1) + ' hours ago';
   } else {
-    days = dateDiff / oneDay;
-    if (days <100){
-      timeAgo = (days).toFixed(1) + ' days ago'
-    }
-    else {
-      timeAgo = (days).toFixed(0) + ' days ago'
+    var days = dateDiff / oneDay;
+    if (days < 100) {
+      timeAgo = (days).toFixed(1) + ' days ago';
+    } else {
+      timeAgo = (days).toFixed(0) + ' days ago';
     }
   }
-  return timeAgo
+  return timeAgo;
 }
 
 function ToLongString (date) {
   var monthNames = [
-      "January", "February", "March",
-      "April", "May", "June", "July",
-      "August", "September", "October",
-      "November", "December"
-    ];
+    'January', 'February', 'March',
+    'April', 'May', 'June', 'July',
+    'August', 'September', 'October',
+    'November', 'December'
+  ];
 
-  var day = ('0' +  date.getDate()).slice(-2);
+  var day = ('0' + date.getDate()).slice(-2);
   var monthIndex = date.getMonth();
   var year = date.getFullYear();
 
@@ -35,68 +34,71 @@ function ToLongString (date) {
 
 function GetRepo (Url, ClassName) {
   $.getJSON(Url).done(function (json) {
-    date = new Date(json.pushed_at)
+    var date = new Date(json.pushed_at);
 
-    $('.last-push-' + ClassName).html(ToLongString(date))
-    $('.last-push-' + ClassName).fadeIn(0)
-  })
+    $('.last-push-' + ClassName).html(ToLongString(date));
+    $('.last-push-' + ClassName).fadeIn(0);
+  });
 }
 
 function GetLatestReleaseInfo (Url, ClassName) {
   $.getJSON(Url).done(function (json) {
-
-    var totalDownloadCount = 0
+    var totalDownloadCount = 0;
     if (json.length !== 0) {
       for (var i1 = 0; i1 < json.length; i1++) {
-          var count = 0
-          for (var i2 = 0; i2 < json[i1].assets.length; i2++) {
-            count += json[i1].assets[i2].download_count
-          }
-          totalDownloadCount += count
+        var count = 0;
+        for (var i2 = 0; i2 < json[i1].assets.length; i2++) {
+          count += json[i1].assets[i2].download_count;
+        }
+        totalDownloadCount += count;
       }
 
-      var release = json[0]
-      var downlink = ''
+      var release = json[0];
+      var downlink = '';
       if (release.assets) {
         for (var i = 0; i < release.assets.length; i++) {
-          var size = release.assets[i].size
-          var sizeb = ''
+          var size = release.assets[i].size;
+          var sizeb = '';
           if (size < 800) {
-            sizeb = ' B'
+            sizeb = ' B';
           } else {
-            size /= 1024
+            size /= 1024;
             if (size < 800) {
-              sizeb = ' KiB'
+              sizeb = ' KiB';
             } else {
-              size /= 1024
-              sizeb = ' MiB'
+              size /= 1024;
+              sizeb = ' MiB';
             }
           }
 
-          if      (size >= 100) { size = size.toFixed(0) }
-          else if (size >= 10)  { size = size.toFixed(1) }
-          else                  { size = size.toFixed(2) }
+          if (size >= 100) {
+            size = size.toFixed(0);
+          } else if (size >= 10) {
+            size = size.toFixed(1);
+          } else {
+            size = size.toFixed(2);
+          }
 
-          downlink += '<nobr><a href = "' + release.assets[i].browser_download_url + '">' + 'Download.' + release.assets[i].name.split('.').pop() + '</a> ' + '(' + size.toString() + sizeb + ')</nobr><br>'
+          downlink += '<nobr><a href = "' + release.assets[i].browser_download_url + '">' + 'Download.' + release.assets[i].name.split('.').pop() + '</a> ' + '(' + size.toString() + sizeb + ')</nobr><br>';
 
-          if (i !== release.assets.length - 1) downlink += '\n'
+          if (i !== release.assets.length - 1) downlink += '\n';
         }
       }
 
-      $('.version-' + ClassName).html(release.tag_name)
-      $('.version-' + ClassName).fadeIn(0)
+      $('.version-' + ClassName).html(release.tag_name);
+      $('.version-' + ClassName).fadeIn(0);
 
-      $('.download-count-' + ClassName).html(totalDownloadCount.toString())
-      $('.download-count-' + ClassName).fadeIn(0)
+      $('.download-count-' + ClassName).html(totalDownloadCount.toString());
+      $('.download-count-' + ClassName).fadeIn(0);
 
-      if (downlink){
-        $('.release-links-' + ClassName).html(downlink)
-        $('.release-links-' + ClassName).fadeIn(0)
+      if (downlink) {
+        $('.release-links-' + ClassName).html(downlink);
+        $('.release-links-' + ClassName).fadeIn(0);
 
-        var timeAgo = TimeAgo (new Date(release.published_at), new Date())
-        $('.release-date-' + ClassName).html('<nobr>(Released ' + timeAgo + ')</nobr>')
-        $('.release-date-' + ClassName).fadeIn(0)
+        var timeAgo = TimeAgo(new Date(release.published_at), new Date());
+        $('.release-date-' + ClassName).html('<nobr>(Released ' + timeAgo + ')</nobr>');
+        $('.release-date-' + ClassName).fadeIn(0);
       }
     }
-  })
+  });
 }
--- scripts/overlaps.js
+++ scripts/overlaps.js
@@ -1,22 +1,53 @@
 var overlaps = (function () {
-    function getPositions( elem ) {
-        var pos, width, height;
-        pos = $( elem ).position();
-        width = $( elem ).width();
-        height = $( elem ).height();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    }
+  function getPositions (elem) {
+    var pos, width, height;
+    pos = $(elem).position();
+    width = $(elem).width();
+    height = $(elem).height();
+    return [
+      [pos.left, pos.left + width],
+      [pos.top, pos.top + height]
+    ];
+  }
 
-    function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
+  function comparePositions (p1, p2) {
+    var r1, r2;
+    r1 = p1[0] < p2[0] ? p1 : p2;
+    r2 = p1[0] < p2[0] ? p2 : p1;
+    return r1[1] > r2[0] || r1[0] === r2[0];
+  }
 
-    return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
-        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-    };
+  return function (a, b) {
+    var pos1 = getPositions(a);
+    var pos2 = getPositions(b);
+    return comparePositions(pos1[0], pos2[0]) && comparePositions(pos1[1], pos2[1]);
+  };
 })();
+
+var waitForFinalEvent = (function () {
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout(timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
+
+function check () {
+  var top = document.getElementsByClassName('container')[0];
+  var bottom = document.getElementsByClassName('container sidebar-sticky')[0];
+  var sidebar = document.getElementsByClassName('sidebar')[0];
+
+  if (overlaps(top, bottom)) {
+    sidebar.innerHTML = sidebar.innerHTML.replace(new RegExp('</div>\\s+<div class="container sidebar-sticky">', 'm'), '<br><!--overlaps-->');
+  } else {
+    sidebar.innerHTML = sidebar.innerHTML.replace(new RegExp('<br><!--overlaps-->', 'g'), '</div>\\n<div class="container sidebar-sticky">');
+  }
+}
+
+check();
+$(window).resize(function () { waitForFinalEvent(check, 70, 'some unique string'); });
